2ab8a416889104cf877914b677b51c8c3fdbb67d
Craig <bleepy@gmail.com>
2021-03-09 00:48:59 +0000

    Reduce chance of skeletons dropping bows to 25%

--- src/main/java/eu/genesismc/genesissmp/PreventPickup.java
+++ src/main/java/eu/genesismc/genesissmp/PreventPickup.java
@@ -1,9 +1,15 @@
 package eu.genesismc.genesissmp;
 
+import org.bukkit.Material;
 import org.bukkit.entity.Skeleton;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.event.entity.EntityPickupItemEvent;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.Random;
+
 
 public class PreventPickup implements Listener {
 
@@ -12,6 +18,16 @@ public class PreventPickup implements Listener {
         if (e.getEntity() instanceof Skeleton) {
             e.setCancelled(true);
             e.getEntity().setCanPickupItems(false);
+        }
+    }
+
+    @EventHandler
+    public void drop(EntityDeathEvent e) {
+        if (e.getEntity() instanceof Skeleton) {
+            Random r = new Random();
+            if (r.nextInt(100)+1 > 25) {
+                e.getDrops().remove(new ItemStack(Material.BOW, 1));
+            }
         }
     }
 
