94bf2cf37a0ef8cee99b251d866ecc6bd3ba266b
Craig <bleepy@gmail.com>
2021-07-17 22:49:14 +0100

    Notify staff on attempted spawner renaming

--- src/main/java/eu/genesismc/genesissmp/events/AnvilInventory.java
+++ src/main/java/eu/genesismc/genesissmp/events/AnvilInventory.java
@@ -1,6 +1,7 @@
 package eu.genesismc.genesissmp.events;
 
 import net.md_5.bungee.api.ChatColor;
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -13,9 +14,15 @@ public class AnvilInventory implements Listener {
     public void onAnvil(InventoryClickEvent e) {
         if (e.getCurrentItem() == null) { return; }
         if (e.getInventory().getType() == InventoryType.ANVIL) {
-            if (e.getCurrentItem().getType() == Material.SPAWNER) {
+            if (e.getCurrentItem().getType() == Material.SPAWNER && !e.getWhoClicked().isOp()) {
                 e.setCancelled(true);
-                e.getWhoClicked().sendMessage(ChatColor.RED + "You cannot rename spawners.");
+                e.getWhoClicked().sendMessage(ChatColor.RED + "Sorry, spawners cannot be renamed by players.");
+                //try { e.getWhoClicked().closeInventory(); } catch (Exception ex) { /*ignore*/ }
+                String msg = ChatColor.translateAlternateColorCodes('&',
+                        "&3[&eGenesisSMP&3] Prevented &e" + e.getWhoClicked().getName() + " &3from &erenaming a spawner&3 on an anvil."
+                );
+                Bukkit.broadcast(msg, "illegalstack.notify");
+
             }
         }
     }
