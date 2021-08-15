cb79452e9b9ad73215b8b34a4d95516be377273c
Craig <bleepy@gmail.com>
2021-08-15 22:07:19 +0100

    missing comma

--- src/main/java/eu/genesismc/genesissmp/commands/PlotCommand.java
+++ src/main/java/eu/genesismc/genesissmp/commands/PlotCommand.java
@@ -393,7 +393,7 @@ public class PlotCommand implements CommandExecutor, Listener, TabCompleter {
                             "Polished_Deepslate",
                             "Rooted_Dirt",
                             "Sand",
-                            "Smooth_Stone"
+                            "Smooth_Stone",
                             "Snow_Block",
                             "Stone",
                             "Tuff",
